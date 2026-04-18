6f5279aa9bedafb4e346e21e79ccb74f50da8d3b
Guillaume Friloux <guillaume@friloux.me>
2026-04-18 09:47:00 +0200

    test(fmt): use 2 spaces
    use 2 spaces
    
    Signed-off-by: Guillaume Friloux <guillaume@friloux.me>

--- checks/rustfmt/default.nix
+++ checks/rustfmt/default.nix
@@ -3,6 +3,6 @@ pkgs.runCommand "rustfmt-check" {
   nativeBuildInputs = [pkgs.rustfmt];
 } ''
   cp -r ${../../src} ./src
-  rustfmt --check ./src/lib.rs ./src/metadata.rs
+  rustfmt --check --config tab_spaces=2 ./src/lib.rs ./src/metadata.rs
   touch $out
 ''
